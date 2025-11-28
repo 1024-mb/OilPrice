2ea6b42bfd94285ad4831adf892c50853fddc012
1024-mb <m.sajjad.2007.jan@gmail.com>
2025-11-29 06:25:57 +0800

    added comments, optimised flow and added more content to graph

--- getPriceData.sh
+++ getPriceData.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
+# opens website (preparing to fetch oil prices)
 curl -o "response.html" "https://oilprice.com/"
 
+# gets the current record index and increments it - this is to be used as the primary key
 index=$(cat index.txt | awk '{print $1+0}')
 index=$((index + 1))
 echo "$index" > "index.txt"
 
+#gets four prices corresponding to WTI, Brent and Murban at the current time.
 prices_str=($(grep -m 5 'class="value"' 'response.html' | awk -F '<[^>]*>' '{print $2}'))
 prices=()
 price1=0
 
+# converts the prices from string to integer and adds it to a prices list
 for price in "${prices_str[@]}"; do
 	price1=$(echo "$price" | awk '{print $1+0.0}')
 	prices+=($price1)
 done
 
 
+#gets current date and time, to be used to filter records in the database
 datetime=$(date +"%H:%M:%S")
 date=$((date +"%D/%M/%Y") | awk -F "/" '{ print $5 "-" $1 "-" $2 }')
-echo "$date"
 
-mysql -u "${moiz}" -p"${MYSQLPASS}" "${CW_1314}" <<EOFMYSQL
+#inserts the data for the fuel prices, as well as the timestamp into the OILPRICES table
+mysql -u "moiz" -p"${MYSQLPASS}" "CW_1314" <<EOFMYSQL
 INSERT INTO CW_1314.OILPRICES(RecordID, WTI, Brent, Murban, TimeReading, DateReading) 
 VALUES ($index, ${prices[0]}, ${prices[1]}, ${prices[2]}, '$datetime', '$date');
 EOFMYSQL
+ 
--- plotGraph.sh
+++ plotGraph.sh
@@ -2,10 +2,11 @@
 
 #Gets day+month+year for title of graph
 datetime=$(date +"%a, %B %d")
-datedisplay=$(echo "$datetime")
-date=$((date +"%D/%M/%Y") | awk -F "/" '{ print $5 "-" $1 "-" $2 }')
+datedisplay=$(/usr/bin/echo "$datetime")
+date=$((/usr/bin/date +"%D/%M/%Y") | /usr/bin/awk -F "/" '{ print $5 "-" $1 "-" $2 }')
 
-BRENTdata=$(mysql -u "${moiz}" -p"${MYSQLPASS}" "${CW_1314}" <<EOFMYSQL
+# gets the max, min and current brent prices
+BRENTdata=$(/usr/bin/mysql -u "moiz" -p"${MYSQLPASS}" "CW_1314" <<EOFMYSQL
               (
               SELECT BRENT FROM CW_1314.OILPRICES
               WHERE DateReading = '${date}'
@@ -29,12 +30,15 @@ BRENTdata=$(mysql -u "${moiz}" -p"${MYSQLPASS}" "${CW_1314}" <<EOFMYSQL
 EOFMYSQL
 )
 
-BRENTdata=$(echo "$BRENTdata" | grep -v "BRENT")
-BRENTLatest=$(echo "$BRENTdata" | awk 'NR==1 {print}')
-BRENTHighest=$(echo "$BRENTdata" | awk 'NR==2 {print}')
-BRENTLowest=$(echo "$BRENTdata" | awk 'NR==3 {print}')
+# parses the data for Brent
+BRENTdata=$(/usr/bin/echo "$BRENTdata" | /usr/bin/grep -v "BRENT")
+BRENTLatest=$(/usr/bin/echo "$BRENTdata" | /usr/bin/awk 'NR==1 {print}')
+BRENTHighest=$(/usr/bin/echo "$BRENTdata" | /usr/bin/awk 'NR==2 {print}')
+BRENTLowest=$(/usr/bin/echo "$BRENTdata" | /usr/bin/awk 'NR==3 {print}')
 
-WTIdata=$(mysql -u "${moiz}" -p"${MYSQLPASS}" "${CW_1314}" <<EOFMYSQL
+
+# extracts the latest data for WTI from OILPRICES
+WTIdata=$(/usr/bin/mysql -u "moiz" -p"${MYSQLPASS}" "CW_1314" <<EOFMYSQL
               (
               SELECT WTI FROM CW_1314.OILPRICES
               WHERE DateReading = '${date}'
@@ -58,13 +62,15 @@ WTIdata=$(mysql -u "${moiz}" -p"${MYSQLPASS}" "${CW_1314}" <<EOFMYSQL
 EOFMYSQL
 )
 
-WTIdata=$(echo "$WTIdata" | grep -v "WTI")
-WTILatest=$(echo "$WTIdata" | awk 'NR==1 {print}')
-WTIHighest=$(echo "$WTIdata" | awk 'NR==2 {print}')
-WTILowest=$(echo "$WTIdata" | awk 'NR==3 {print}')
+# parses this data
+WTIdata=$(/usr/bin/echo "$WTIdata" | /usr/bin/grep -v "WTI")
+WTILatest=$(/usr/bin/echo "$WTIdata" | /usr/bin/awk 'NR==1 {print}')
+WTIHighest=$(/usr/bin/echo "$WTIdata" | /usr/bin/awk 'NR==2 {print}')
+WTILowest=$(/usr/bin/echo "$WTIdata" | /usr/bin/awk 'NR==3 {print}')
 
 
-MURBANdata=$(mysql -u "${moiz}" -p"${MYSQLPASS}" "${CW_1314}" <<EOFMYSQL
+#gets the data for Murban from OILPRICES
+MURBANdata=$(/usr/bin/mysql -u "moiz" -p"${MYSQLPASS}" "CW_1314" <<EOFMYSQL
               (
               SELECT MURBAN FROM CW_1314.OILPRICES
               WHERE DateReading = '${date}'
@@ -88,29 +94,35 @@ MURBANdata=$(mysql -u "${moiz}" -p"${MYSQLPASS}" "${CW_1314}" <<EOFMYSQL
 EOFMYSQL
 )
 
-MURBANdata=$(echo "$MURBANdata" | grep -v "MURBAN")
-MURBANLatest=$(echo "$MURBANdata" | awk 'NR==1 {print}')
-MURBANHighest=$(echo "$MURBANdata" | awk 'NR==2 {print}')
-MURBANLowest=$(echo "$MURBANdata" | awk 'NR==3 {print}')
+# parses this data
+MURBANdata=$(/usr/bin/echo "$MURBANdata" | /usr/bin/grep -v "MURBAN")
+MURBANLatest=$(/usr/bin/echo "$MURBANdata" | /usr/bin/awk 'NR==1 {print}')
+MURBANHighest=$(/usr/bin/echo "$MURBANdata" | /usr/bin/awk 'NR==2 {print}')
+MURBANLowest=$(/usr/bin/echo "$MURBANdata" | /usr/bin/awk 'NR==3 {print}')
 
-echo "s $MURBANHighest l $MURBANLowest o $MURBANLatest f"
-#Plots data for all fuels
-# add natural gas: \
-#(West Texas Intermediate)
-#     "data_NATURAL_GAS.dat" using 1:2 title "Natural Gas" with linespoints linetype 4
-gnuplot <<EOF
+
+# formats datetime for comparison
+datetime=$(date +"%H:%M:%S")
+
+#if the day is almost over, will add the labels for the times when the stock market closed for
+# Murban and WTI
+
+if [[ "$datetime" < "23:00:00" ]]; then
+/usr/bin/gnuplot <<EOF
+set encoding utf8
+set terminal pngcairo enhanced
 set terminal png size 1000,600
 set output 'image.png'
-set label "TODAY" at graph 1.0225,0.66 textcolor rgbcolor "#0c0d0d" font ",18"
-set label "Murban Latest:   \$${MURBANLatest}" at graph 1.0275,0.6 textcolor rgbcolor "#1c1c1c"
-set label "Murban High:     \$${MURBANHighest}" at graph 1.0275,0.55 textcolor rgbcolor "#1c1c1c"
-set label "Murban Low:      \$${MURBANLowest}" at graph 1.0275,0.5 textcolor rgbcolor "#1c1c1c"
-set label "Brent Latest:      \$${BRENTLatest}" at graph 1.0275,0.4 textcolor rgbcolor "#1c1c1c"
-set label "Brent High:        \$${BRENTHighest}" at graph 1.0275,0.35 textcolor rgbcolor "#1c1c1c"
-set label "Brent Low:         \$${BRENTLowest}" at graph 1.0275,0.30 textcolor rgbcolor "#1c1c1c"
-set label "WTI Latest:        \$${WTILatest}" at graph 1.0275,0.2 textcolor rgbcolor "#1c1c1c"
-set label "WTI High:          \$${WTIHighest}" at graph 1.0275,0.15 textcolor rgbcolor "#1c1c1c"
-set label "WTI Low:           \$${WTILowest}" at graph 1.0275,0.1 textcolor rgbcolor "#1c1c1c"
+set label "TODAY    \$USD" at graph 1.0225,0.66 textcolor rgbcolor "#0c0d0d" font ",18"
+set label "Murban Latest:   \$${MURBANLatest}" at graph 1.0275,0.6 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "Murban High:     \$${MURBANHighest}" at graph 1.0275,0.55 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "Murban Low:      \$${MURBANLowest}" at graph 1.0275,0.5 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "Brent Latest:      \$${BRENTLatest}" at graph 1.0275,0.4 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "Brent High:        \$${BRENTHighest}" at graph 1.0275,0.35 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "Brent Low:         \$${BRENTLowest}" at graph 1.0275,0.30 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "WTI Latest:        \$${WTILatest}" at graph 1.0275,0.2 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "WTI High:          \$${WTIHighest}" at graph 1.0275,0.15 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "WTI Low:           \$${WTILowest}" at graph 1.0275,0.1 textcolor rgbcolor "#1c1c1c" font ",11"
 
 set ylabel "Price Per Barrel / US$" font ",15" offset 1
 set xlabel "Time" font ",15"
@@ -122,12 +134,50 @@ set format x "%H:%M"
 set grid
 
 set key outside
-plot "data_BRENT.dat" using 1:2 title "Brent Crude" with linespoints linetype 1 linewidth 1.5 pointsize 0.9 , \
-     "data_WTI.dat" using 1:2 title "WTI Crude" with linespoints linetype 2 linewidth 1.5 pointsize 0.9, \
-     "data_MURBAN.dat" using 1:2 title "Murban Crude" with linespoints linetype 3 linewidth 1.5 pointsize 0.9
+plot "data_BRENT.dat" using 1:2 title "Brent Crude" with linespoints linetype 4 linewidth 1.5 pointsize 0.9 , \
+     "data_WTI.dat" using 1:2 title "WTI Crude" with linespoints linetype 7 linewidth 1.5 pointsize 0.9, \
+     "data_MURBAN.dat" using 1:2 title "Murban Crude" with linespoints linetype 5 linewidth 1.5 pointsize 0.9
 EOF
 
-WTI=$(mysql -u "${moiz}" -p"${MYSQLPASS}" "${CW_1314}" <<EOFMYSQL
+else
+/usr/bin/gnuplot <<EOF
+set encoding utf8
+set terminal pngcairo enhanced
+set terminal png size 1000,600
+set output 'image.png'
+set label "TODAY    \$USD" at graph 1.0225,0.66 textcolor rgbcolor "#0c0d0d" font ",18"
+set label "Murban Latest:   \$${MURBANLatest}" at graph 1.0275,0.6 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "Murban High:     \$${MURBANHighest}" at graph 1.0275,0.55 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "Murban Low:      \$${MURBANLowest}" at graph 1.0275,0.5 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "Brent Latest:      \$${BRENTLatest}" at graph 1.0275,0.4 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "Brent High:        \$${BRENTHighest}" at graph 1.0275,0.35 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "Brent Low:         \$${BRENTLowest}" at graph 1.0275,0.30 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "WTI Latest:        \$${WTILatest}" at graph 1.0275,0.2 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "WTI High:          \$${WTIHighest}" at graph 1.0275,0.15 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "WTI Low:           \$${WTILowest}" at graph 1.0275,0.1 textcolor rgbcolor "#1c1c1c" font ",11"
+set label "Murban Closes (11:30)" at graph 0.5,0.05 textcolor rgbcolor "#1c1c1c" font ",7"
+set label "WTI Closes (22:30)" at graph 0.75,0.05 textcolor rgbcolor "#1c1c1c" font ",7"
+
+
+set ylabel "Price Per Barrel / US$" font ",15" offset 1
+set xlabel "Time" font ",15"
+
+set timefmt "%H:%M:%S"
+set title "$datedisplay - Oil Prices" offset 10,0.0 font ",20"
+set xdata time
+set format x "%H:%M"
+set grid
+
+set key outside
+plot "data_BRENT.dat" using 1:2 title "Brent Crude" with linespoints linetype 4 linewidth 1.5 pointsize 0.9 , \
+     "data_WTI.dat" using 1:2 title "WTI Crude" with linespoints linetype 7 linewidth 1.5 pointsize 0.9, \
+     "data_MURBAN.dat" using 1:2 title "Murban Crude" with linespoints linetype 5 linewidth 1.5 pointsize 0.9
+EOF
+
+fi
+
+# extracts and puts the next set of price and time values into the .dat file for future reading
+WTI=$(/usr/bin/mysql -u "moiz" -p"${MYSQLPASS}" "CW_1314" <<EOFMYSQL
               SELECT WTI, TimeReading FROM CW_1314.OILPRICES
               WHERE DateReading = '${date}'
               ORDER BY TimeReading DESC
@@ -135,12 +185,12 @@ WTI=$(mysql -u "${moiz}" -p"${MYSQLPASS}" "${CW_1314}" <<EOFMYSQL
 EOFMYSQL
 )
 
-WTI=$(echo "$WTI" | grep -v "WTI	TimeReading")
-echo "$WTI" | grep -v "WTI	TimeReading" | awk '{ printf "%s%s %s\n", $2, $3, $1 }' > "data_WTI.dat"
+WTI=$(/usr/bin/echo "$WTI" | /usr/bin/grep -v "WTI	TimeReading")
+/usr/bin/echo "$WTI" | /usr/bin/grep -v "WTI	TimeReading" | /usr/bin/awk '{ printf "%s%s %s\n", $2, $3, $1 }' > "data_WTI.dat"
 
 
-
-Brent=$(mysql -u "${moiz}" -p"${MYSQLPASS}" "${CW_1314}" <<EOFMYSQL
+# extracts and puts the next set of price and time values into the .dat file for future reading
+Brent=$(/usr/bin/mysql -u "moiz" -p"${MYSQLPASS}" "CW_1314" <<EOFMYSQL
               SELECT Brent, TimeReading FROM CW_1314.OILPRICES
               WHERE DateReading = '${date}'
               ORDER BY TimeReading DESC
@@ -148,12 +198,12 @@ Brent=$(mysql -u "${moiz}" -p"${MYSQLPASS}" "${CW_1314}" <<EOFMYSQL
 EOFMYSQL
 )
 
-Brent=$(echo "$Brent" | grep -v "Brent TimeReading")
-echo "$Brent" | grep -v "Brent	TimeReading" | awk '{ printf "%s%s %s\n", $2, $3, $1 }' > "data_BRENT.dat"
+Brent=$(/usr/bin/echo "$Brent" | /usr/bin/grep -v "Brent TimeReading")
+/usr/bin/echo "$Brent" | /usr/bin/grep -v "Brent	TimeReading" | /usr/bin/awk '{ printf "%s%s %s\n", $2, $3, $1 }' > "data_BRENT.dat"
 
 
-
-Murban=$(mysql -u "${moiz}" -p"${MYSQLPASS}" "${CW_1314}" <<EOFMYSQL
+# extracts and puts the next set of price and time values into the .dat file for future reading
+Murban=$(/usr/bin/mysql -u "moiz" -p"${MYSQLPASS}" "CW_1314" <<EOFMYSQL
               SELECT Murban, TimeReading FROM CW_1314.OILPRICES
               WHERE DateReading = '${date}'
               ORDER BY TimeReading DESC
@@ -161,18 +211,7 @@ Murban=$(mysql -u "${moiz}" -p"${MYSQLPASS}" "${CW_1314}" <<EOFMYSQL
 EOFMYSQL
 )
 
-Murban=$(echo "$Murban" | grep -v "Murban TimeReading")
-echo "$Murban" | grep -v "Murban	TimeReading" | awk '{ printf "%s%s %s\n", $2, $3, $1 }' > "data_MURBAN.dat"
+Murban=$(/usr/bin/echo "$Murban" | /usr/bin/grep -v "Murban TimeReading")
+/usr/bin/echo "$Murban" | /usr/bin/grep -v "Murban	TimeReading" 
+/usr/bin/echo "$Murban" | /usr/bin/grep -v "Murban	TimeReading" | /usr/bin/awk '{ printf "%s%s %s\n", $2, $3, $1 }' > "data_MURBAN.dat"
 
-
-# gnuplot unset key
-# plot "data.dat" title "yah blah" -sets the title for the graph
-# linetype <value> linecolor <value(RGB)> with linespoints - for line with points
-#                                         lines for just lines (no plotting)
-# plot multiple graphs with commas in between
-# set xrange[min:max] - sets the range on the x axis
-# set xlabel "yah"
-# set ylabel "yah"
-# set terminal png size 800,600
-# set ouput "image.png"
-# replot to 
